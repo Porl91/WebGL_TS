b18526f24ec963081e8af0d690634e5a923d3086
Paul Riding <paul.riding@newmind.co.uk>
2019-03-12 09:01:33 +0000

    Committing gh-pages

--- src/drawable.ts
+++
@@ -1,6 +0,0 @@
-import { mat4 } from "gl-matrix";
-import { ProgramInfo } from "./program_info";
-
-export interface Drawable {
-    draw(programInfo: ProgramInfo, viewProjectionMatrix: mat4): void;
-}
